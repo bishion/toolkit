44a6ca8b9efbb7bd12af4e179d12406872902da9
guofangbi <guo.fangbi@oakhealthcare.cn>
2022-11-19 20:09:05 +0800

    feat: demo添加对mybatis的支持

--- sample-demo/src/main/java/cn/bishion/toolkit/sample/SampleDemoApplication.java
+++ sample-demo/src/main/java/cn/bishion/toolkit/sample/SampleDemoApplication.java
@@ -1,5 +1,6 @@
 package cn.bishion.toolkit.sample;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients
+@MapperScan({ "cn.bishion.toolkit.sample.db.mapper" })
 public class SampleDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SampleDemoApplication.class, args);
---
+++ sample-demo/src/main/java/cn/bishion/toolkit/sample/web/MybatisController.java
@@ -0,0 +1,36 @@
+package cn.bishion.toolkit.sample.web;
+
+import cn.bishion.toolkit.sample.db.domain.UserInfo;
+import cn.bishion.toolkit.sample.db.mapper.UserInfoMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class MybatisController {
+
+    @Resource
+    private UserInfoMapper userInfoMapper;
+
+    @GetMapping("/user/insert")
+    public String insert() {
+        UserInfo userInfo = new UserInfo();
+
+        userInfo.setLoginName("testLoginName");
+        userInfo.setPassword("testPassword");
+        userInfo.setUsername("testUsername");
+
+        int insert = userInfoMapper.insert(userInfo);
+        return String.valueOf(insert);
+    }
+    @GetMapping("/user/update")
+    public String update() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setVersion(2);
+        userInfo.setUsername("testUpdate");
+        userInfo.setId(5L);
+        int insert = userInfoMapper.updateById(userInfo);
+        return String.valueOf(insert);
+    }
+}
