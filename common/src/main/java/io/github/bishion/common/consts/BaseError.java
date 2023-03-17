04fe48214140848097530a902d1e816f65b24519
guofangbi <guo.fangbi@oakhealthcare.cn>
2023-03-17 15:23:13 +0800

    feat: 代码整理

--- common/src/main/java/io/github/bishion/common/consts/BaseError.java
+++ common/src/main/java/io/github/bishion/common/consts/BaseError.java
@@ -2,7 +2,6 @@ package io.github.bishion.common.consts;
 
 import cn.hutool.core.util.StrUtil;
 
-import java.text.MessageFormat;
 import java.util.Objects;
 
 /**
@@ -13,7 +12,7 @@ import java.util.Objects;
  * @since 2022-05-24 16:29:02
  */
 public interface BaseError {
-    Object[] EMPTY_ARR = new String[]{BaseConst.EMPTY};
+    Object[] EMPTY_ARR = new String[]{BaseConst.EMPTY, BaseConst.EMPTY, BaseConst.EMPTY, BaseConst.EMPTY, BaseConst.EMPTY};
 
     /**
      * 错误代码
@@ -40,13 +39,6 @@ public interface BaseError {
             params = EMPTY_ARR;
         }
 
-        MessageFormat msgFmt = new MessageFormat(getPattern());
-        return msgFmt.format(params);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(StrUtil.format("{},{}", 123, "123", "123"));
-        MessageFormat msgFmt = new MessageFormat("hehe{0},{1},{2}");
-        System.out.println(msgFmt.format(null));
+        return StrUtil.format(getPattern(), params);
     }
 }
---
+++ sample-demo/demo01-pangolin/src/main/java/io/github/bishion/demo/pangolin/PangolinApplication.java
@@ -0,0 +1,11 @@
+package io.github.bishion.demo.pangolin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangolinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PangolinApplication.class, args);
+    }
+}
