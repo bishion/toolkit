3ec6cd64a00310c0877ce0d3c3264d183e3b3c25
guofangbi <guo.fangbi@oakhealthcare.cn>
2023-07-04 14:01:36 +0800

    feat: 代码整理

--- common/src/main/java/io/github/bishion/common/consts/JsonError.java
+++ common/src/main/java/io/github/bishion/common/consts/JsonError.java
@@ -2,7 +2,7 @@ package io.github.bishion.common.consts;
 
 public enum JsonError implements BaseError {
 
-    TO_STR_ERROR("JSN001", "对象json失败:{0}"),
+    TO_STR_ERROR("JSN001", "对象转json失败:{0}"),
 
     TO_BEAN_ERROR("JSN002", "json转对象失败:{0}");
 
